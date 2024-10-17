2ab75b3e6563cb84338967ba40a5001bcf6e63af
Pasha Rodideal <pasharodideal@gmail.com>
2024-10-16 20:19:47 -0400

    task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,6 +1,7 @@
 '''Using the Emotion Predict function of the Watson NLP Library 
 to create the emotion detector function.
 '''
+import json
 import requests
 
 def emotion_detector(text_to_analyze):
@@ -13,4 +14,9 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     newobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = newobj, headers = headers)
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions, key=emotions.get)
+    emotions['dominant emotion'] = dominant_emotion
+    print(emotions)
